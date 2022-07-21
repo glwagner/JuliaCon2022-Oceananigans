27b1995a4460e48650ffbbc1bd2ffb384127d600
Gregory Wagner <wagner.greg@gmail.com>
2022-07-21 14:36:59 -0600

    Add Makie, spruce up visualization, make a movie

--- ocean_convection.jl
+++ ocean_convection.jl
@@ -1,10 +1,17 @@
 using Oceananigans
+using Oceananigans.Units
 using GLMakie
 
-# Model
-grid = RectilinearGrid(size=(64, 64), x=(0, 128), z=(-64, 0),
+#####
+##### Model setup
+#####
+
+# A two-dimensional grid
+grid = RectilinearGrid(size=(128, 128), x=(0, 128), z=(-64, 0),
                        topology=(Periodic, Flat, Bounded))
 
+# Nonhydrotatic model of a buoyancy, viscous fluid with
+# high-order numerics for advection
 model = NonhydrostaticModel(; grid,
                             timestepper = :RungeKutta3,
                             advection = WENO5(),
@@ -12,38 +19,83 @@ model = NonhydrostaticModel(; grid,
                             closure = ScalarDiffusivity(ν=1e-1, κ=1e-1),
                             buoyancy = BuoyancyTracer())
 
-Δb = 1
-step(z, z₀, δ) = 1/2 * (tanh((z - z₀) / δ) + 1)
-bᵢ(x, y, z) = - Δb * step(z, -32, 4) + 1e-2 * ϵ(x, y, z) 
+# Initial condition: cold fluid over hot fluid
+z₀ = -32 # m
+δz = 4 # m
+Δb = 1 # m s⁻²
 ϵ(x, y, z) = randn()
+bᵢ(x, y, z) = - Δb * tanh((z - z₀) / δz) + 1e-2 * ϵ(x, y, z) 
 set!(model, b=bᵢ)
 
-# Simulation
-simulation = Simulation(model, Δt=1e-1, stop_time=200)
+#####
+##### Simulation
+#####
 
-progress(sim) = @info string("Iter: ", iteration(sim))
+# Simulation with initial time-step of 0.1 seconds
+simulation = Simulation(model, Δt=0.1, stop_time=2minutes)
+
+# Adaptive time-stepping
+wizard = TimeStepWizard(cfl=0.5)
+simulation.callbacks[:wizard] = Callback(wizard, IterationInterval(10))
+
+# Logging via callback
+progress(sim) = @info string("Iter: ", iteration(sim),
+                             ", time: ", prettytime(sim),
+                             ", Δt: ", prettytime(sim.Δt))
+
 simulation.callbacks[:progress] = Callback(progress, IterationInterval(10))
 
+# Some non-trivial output: buoyancy and vorticity
 filename = "ocean_convection.jld2"
-outputs = model.tracers
+
+u, v, w = model.velocities
+ξ = ∂z(w) - ∂x(u) # abstract operation
+
+b = model.tracers.b
+
+outputs = (; b, ξ)
+
 simulation.output_writers[:jld2] = JLD2OutputWriter(model, outputs; filename,
                                                     schedule = TimeInterval(0.1),
                                                     overwrite_existing = true)
 
 run!(simulation)
 
-# Analysis
+#####
+##### Analysis / visualization with Makie
+#####
+
 bt = FieldTimeSeries(filename, "b")
+ξt = FieldTimeSeries(filename, "ξ")
 t = bt.times
 Nt = length(t)
 
-fig = Figure(resolution=(1200, 800))
-ax = Axis(fig[1, 1], xlabel="x", ylabel="z")
-slider = Slider(fig[2, 1], range=1:Nt, startvalue=1)
+fig = Figure(resolution=(1100, 1200))
+axb = Axis(fig[2, 1], xlabel="x (m)", ylabel="z (m)", title="Buoyancy", aspect=2)
+axξ = Axis(fig[3, 1], xlabel="x (m)", ylabel="z (m)", title="Vorticity", aspect=2)
+
+slider = Slider(fig[4, 1:2], range=1:Nt, startvalue=1)
 n = slider.value
 
+title = @lift string("Convective instability at t = ", prettytime(t[$n]))
+Label(fig[1, 1:2], title, tellwidth=false)
+
 b = @lift interior(bt[$n], :, 1, :)
-heatmap!(ax, b)
+ξ = @lift interior(ξt[$n], :, 1, :)
+
+x, y, z = nodes(bt)
+hm = heatmap!(axb, x, z, b, colormap=:thermal, colorrange=(-0.5, 0.5))
+Colorbar(fig[2, 2], hm)
+
+x, y, z = nodes(ξt)
+ξlim = maximum(abs, interior(ξt)) / 2
+hm = heatmap!(axξ, x, z, ξ, colormap=:balance, colorrange=(-ξlim, ξlim))
+Colorbar(fig[3, 2], hm)
 
 display(fig)
+
+record(fig, "ocean_convection.mp4", 1:Nt, framerate=24) do nn
+    @info "Drawing frame $nn of $Nt..."
+    n[] = nn
+end
 
